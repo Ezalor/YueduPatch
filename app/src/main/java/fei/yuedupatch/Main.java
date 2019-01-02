41dfbf8088c53f90b11d6003194123947555daf9
Ezalor <gggghhhh2924@163.com>
2019-01-02 19:27:10 +0800

    add a mark

--- app/src/main/java/fei/yuedupatch/Main.java
+++ app/src/main/java/fei/yuedupatch/Main.java
@@ -1,11 +1,13 @@
 package fei.yuedupatch;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class Main implements IXposedHookLoadPackage {
+public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 
@@ -21,4 +23,12 @@ public class Main implements IXposedHookLoadPackage {
                 });
     }
 
+        @Override
+        public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+
+            if (!resparam.packageName.equals("com.gedoor.monkeybook"))
+                return;
+
+            resparam.res.setReplacement("com.gedoor.monkeybook", "string", "about_description", "这是一款开源的阅读软件，你可以fork我们的代码自己编译APK。欢迎提交代码帮助改善应用。(P)");
+        }
 }
